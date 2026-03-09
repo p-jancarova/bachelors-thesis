2666e8e9cadd32eb83e7486221dc0a6c21746777
p-jancarova <125305106+p-jancarova@users.noreply.github.com>
2026-03-09 19:57:10 +0100

    Updated the site
    - Added a short description
    - Added references
    - Added the introductory presentation
    - Updated the progress journal

--- script.js
+++ script.js
@@ -2,28 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
 	window.addEventListener("load", function () {
 		document.body.classList.remove("no-transition");
 	});
-	
-	const header = document.querySelector("header");
-
-	const ON_AT = 60;
-	const OFF_AT = 10;
-
-	let isCompact = false;
-
-	function handleScroll() {
-		const y = window.scrollY;
-
-		if (!isCompact && y > ON_AT) {
-			isCompact = true;
-			header.classList.add("compact");
-		} else if (isCompact && y < OFF_AT) {
-			isCompact = false;
-			header.classList.remove("compact");
-		}
-	}
-
-	window.addEventListener("scroll", handleScroll, { passive: true });
-	handleScroll();
   
 	var coll = document.getElementsByClassName("week");
 
